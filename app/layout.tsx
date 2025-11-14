525299e6e427c4b056df8b41a7ed7f0686b9d6dd
csiemichelin <ccc44174@gmail.com>
2025-11-14 11:25:49 +0800

    add supabase setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${crimsonText.className} font-sans antialiased`}>
+      <body className={`${crimsonText.className} font-sans antialiased suppressHydrationWarning`}>
         {children}
         <Analytics />
       </body>
